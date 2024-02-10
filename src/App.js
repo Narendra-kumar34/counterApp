f66fb0cd72205fca640abc9b457c26238e2ec0bf
Narendra <narendrach34@gmail.com>
2024-02-11 00:14:15 +0530

    implemented

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
 
 function App() {
+  const [cnt, setCnt] = useState(0);
+
+  const handleInc = () =>{
+    setCnt((prevCnt) => prevCnt + 1);
+  }
+
+  const handleDec = () =>{
+    setCnt((prevCnt) => prevCnt - 1);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Counter App</h1>
+      <p>Count: {cnt}</p>
+      <button onClick={handleInc}>Increment</button>
+      <button onClick={handleDec}>Decrement</button>
     </div>
   );
 }
